cc8ea69d10cbfd1e07c995da5d63a4f92e984799
waterwindwood <469798015@qq.com>
2016-07-24 11:58:24 +0800

    project新建成功

---
+++ qfs_web/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for qfs_web project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qfs_web.settings")
+
+application = get_wsgi_application()
